f0a65885b0441cb80509b714b400569dd052fcd3
Jeff Labonte <jeff.labonte12@gmail.com>
2020-06-13 02:04:31 -0400

    Simple raindrop exercise

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut noises = String::new();
+    let mut no_factor = true;
+
+    if n % 3 == 0 {
+        no_factor = false;
+        noises = format!("{}Pling", noises);
+    }
+    if n % 5 == 0 {
+        no_factor = false;
+        noises = format!("{}Plang", noises);
+    }
+
+    if n % 7 == 0 {
+        no_factor = false;
+       noises = format!("{}Plong", noises);
+    }
+
+    if no_factor {
+       noises = format!("{}{}", noises, n.to_string());
+    }
+
+    noises
 }
